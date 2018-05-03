6ca10f7e4cecc109717388db81ec06ffca71ffd2
Leian Ivey <leianivey@gmail.com>
2018-05-02 20:39:18 -0700

    Completed test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,155 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import uuid from "uuid";
 
-import './App.css';
+import "./App.css";
 
-import List from './components/list';
-import Input from './components/input';
-import Button from './components/button';
+// Custom Components
+import List from "./components/list";
+import Input from "./components/input";
+import Button from "./components/button";
 
+// Actions
+import * as actions from "./actions";
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <div className="container">
-
-                <h1>Todo List</h1>
-                
-                <div className='add-item-to-list'>
-                    <Input
-                        name='item'
-                        placeholder='New Item...'
-                        onChange={''}
-                    />
-                    <Button onClick={''} type='add'>
-                      Add
-                    </Button>
-                </div>
-
-                <List />
-            </div>
-        );
+    // Default items
+    // Replace with data from TODO Microservice
+    this.state = {
+      newItem: "",
+      items: [{
+        id: 1,
+        message: 'Item 1'
+      }, {
+        id: 2,
+        message: 'Item 2'
+      }, {
+        id: 3,
+        message: 'Item 3'
+      }]
     }
+
+    this.handleNewItem = this.handleNewItem.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
+    this.handleEditItem = this.handleEditItem.bind(this);
+    this.handleDeleteItem = this.handleDeleteItem.bind(this);
+    this.handleUpdateItem = this.handleUpdateItem.bind(this);
+  }
+
+  componentWillMount() {
+    // Start getting all items before the page renders
+    // this.props.getItems();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.items !== nextProps.items) {
+      this.setState({
+        items: nextProps.items
+      })
+    }
+  }
+
+  handleNewItem(message) {
+    this.setState({
+      newItem:  message
+    })
+  }
+
+  handleAddItem() {
+    const {
+      addItems
+    } = this.props;
+
+    const {
+      items,
+      newItem
+    } = this.state;
+
+    const item = {
+      id: uuid.v4(),
+      message: newItem
+    }
+
+    this.setState({
+      items: items.concat(item)
+    })
+    
+    // This is commented out to prevent breaking 
+    // With access to the TODO Microservice 
+    // This request would be  completed and updated in store.
+    
+    // return addItems(item);
+  }
+
+  handleEditItem(id, updates) {
+    const {
+      editItems
+    } = this.props;
+
+    // This is commented out to prevent breaking 
+    // With access to the TODO Microservice 
+    // This request would be  completed and updated in store.
+
+    // return editItems(id, updates);
+  }
+
+  handleDeleteItem(id,) {
+    const {
+      deleteItem
+    } = this.props;
+
+    // This is commented out to prevent breaking 
+    // With access to the TODO Microservice 
+    // This request would be  completed and updated in store.
+
+    // return deleteItem(id);
+  }
+
+  handleUpdateItem(id, message) {
+    this.setState({
+      items: this.state.items.map(item => {
+        if ( id === item.id ) {
+          item.message = message
+        }
+
+        return item;
+      })
+    })
+  }
+
+  render() {
+    const {
+      items
+    } = this.state;
+
+    return (
+      <div className="container">
+        <h1>Todo List</h1>
+
+        <div className="add-item-to-list">
+          <Input name="item" placeholder="New Item..." onChange={ (e) => this.handleNewItem(e) } />
+          <Button onClick={ () => this.handleAddItem() } type="add">
+            Add
+          </Button>
+        </div>
+        
+        <List 
+          items={items}
+          editItem={this.handleEditItem}
+          deleteItem={this.handleEditItem}
+          updateItem={this.handleUpdateItem}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(
+  state => ({
+    items: state.list
+  }),
+  dispatch => bindActionCreators(actions, dispatch)
+)(App);
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,36 +1,92 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import './index.css';
-import Button from '../button';
+import "./index.css";
+import Button from "../button";
+import Input from "../input";
 
 class List extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th width='66%'>Item</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Item</td>
-                        <td>
-                            <Button type='edit' onClick={''}>
-                                Edit
-                            </Button>
-                            <Button type='delete' onClick={''}>
-                                Delete
-                            </Button> 
-                        </td>
-                    </tr>
-                    
-                </tbody>
-            </table>
-        );
+    this.state = {
+      items: this.props.items,
+      isEditing: false
+    };
+
+    this.toggleEdit = this.toggleEdit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.items !== nextProps.items) {
+      this.setState({
+        items: nextProps.items
+      })
     }
+  }
+
+  toggleEdit(id) {
+    this.setState({
+      isEditing: id || false
+    })
+  }
+
+  render() {
+    const { editItem, deleteItem, updateItem } = this.props;
+    const { items, isEditing } = this.state;
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th width="66%">Item</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map(item => (
+            <tr key={item.id}>
+              { isEditing === item.id && 
+                <td>
+                  <Input
+                    name="item"
+                    placeholder={item.message}
+                    onChange={(e) => updateItem(item.id, e)}
+                  />
+                </td>
+              }
+
+              { isEditing !== item.id && 
+                <td onClick={() => item.id && this.toggleEdit(item.id)}>{item.message}</td>
+              }
+
+              <td>
+                <Button
+                  type="edit"
+                  onClick={() => editItem(item.id, item.message)}
+                >
+                  Edit
+                </Button>
+
+                <Button type="delete" onClick={() => deleteItem(item.id)}>
+                  Delete
+                </Button>
+                
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
+
+List.propTypes = {
+  items: PropTypes.array.isRequired,
+  editItem: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  updateItem: PropTypes.func.isRequired
+};
 
 export default List;
